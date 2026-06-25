681b9a0723438b526dbb0118dd850ee80f60c52c
lohead1 <ferrohuanchialvaro@gmail.com>
2026-06-24 21:12:30 -0500

    feat: implementar clase Nodo generica para usar una cola Clase Nodo<T> que sirve para la creacion de la cola, almacenando un dato generico y la referencia al siguiente nodo. Incluye constructor, getters y setters para data y next.

--- src/estructuras_auxiliares/Nodo.java
+++ src/estructuras_auxiliares/Nodo.java
@@ -1,4 +1,22 @@
 package estructuras_auxiliares;
 
-public class Nodo {
+public class Nodo<T> {
+    private T data;
+    private Nodo<T> next;
+    public Nodo(T data,Nodo<T>next){
+        this.data=data;
+        this.next=null;
+    }
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+    public void setNext(Nodo<T> next) {
+        this.next = next;
+    }
+    public Nodo<T> getNext() {
+        return next;
+    }
 }
