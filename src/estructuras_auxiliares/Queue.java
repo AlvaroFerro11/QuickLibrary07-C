fcd6d83f1a9412561fc63105388b6b22ab21e1ae
lohead1 <ferrohuanchialvaro@gmail.com>
2026-06-24 21:54:44 -0500

    fix: ajustar la interfaz Queue Cambie front por peek, quite back y agregue size y mostrar

--- src/estructuras_auxiliares/Queue.java
+++ src/estructuras_auxiliares/Queue.java
@@ -1,11 +1,11 @@
 package estructuras_auxiliares;
 
 import Exceptions.ExceptionIsEmpty;
-
-public interface Queue<E>{
+public interface Queue<E> {
     void enqueue(E x);
-    void dequeue() throws ExceptionIsEmpty;
-    E front() throws ExceptionIsEmpty;
-    E back() throws ExceptionIsEmpty;
+    E dequeue() throws ExceptionIsEmpty;
+    E peek() throws ExceptionIsEmpty;
     boolean isEmpty();
+    int size();
+    void mostrar();
 }
