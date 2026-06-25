cbad363504ce31de0c51c99eab50275e00f014c2
OrlandoMG2004 <orlando.montenegro@estudiante.ucsm.edu.pe>
2026-06-25 13:27:12 -0500

    Creacion de clase principal para el libro usamos implements y comparable para que el libro se compare con otro libro

--- src/estructuras_auxiliares/Libro.java
+++ src/estructuras_auxiliares/Libro.java
@@ -1,5 +1,10 @@
 package estructuras_auxiliares;
 
 public class Libro implements Comparable<Libro>{
-
+    private int code;
+    private String title;
+    private String author;
+    private String category;
+    private String yearPubli;
+    private String  State;
 }
