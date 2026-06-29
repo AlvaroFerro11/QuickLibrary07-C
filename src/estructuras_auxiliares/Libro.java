1ca031bc75facacbb7571c683a46ccacbc12d5e8
OrlandoMG2004 <orlando.montenegro@estudiante.ucsm.edu.pe>
2026-06-29 09:45:56 -0500

    constructor del libro

--- src/estructuras_auxiliares/Libro.java
+++ src/estructuras_auxiliares/Libro.java
@@ -7,5 +7,14 @@ public class Libro implements Comparable<Libro>{
     private String category;
     private String yearPubli;
     private String  State;
+    }
+    public Libro(int codigo, String titulo, String autor, String categoria, int anioPublicacion, Estado estado) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.anioPublicacion = anioPublicacion;
+        this.estado = estado;
+    }
 
 }
