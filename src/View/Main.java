b49713c82584abceeac429b07da5baca0ea588ef
Alvaro <alvaro.montoya.m.m@gmail.com>
2026-06-29 18:37:13 -0500

    Correccion de Main

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,9 +1,13 @@
 package View;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
 
     public static void main(String[] args) {
-        Menu_consola menu = new Menu_consola();
-        menu.iniciar();
+        SwingUtilities.invokeLater(() -> {
+            VentanaBiblioteca ventana = new VentanaBiblioteca();
+            ventana.setVisible(true);
+        });
     }
 }
