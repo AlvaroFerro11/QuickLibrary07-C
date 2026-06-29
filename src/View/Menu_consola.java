dbb62830ca55ef5233d373139f3287534a7e5aa7
Alvaro <alvaro.montoya.m.m@gmail.com>
2026-06-29 17:40:47 -0500

    inicio de codigo

--- src/View/Menu_consola.java
+++ src/View/Menu_consola.java
@@ -1,4 +1,24 @@
 package View;
 
+import java.util.Scanner;
+
+import Controller.GestorBibilioteca;
+import Model.Solicitud;
+import estructuras_auxiliares.Libro;
+
 public class Menu_consola {
+
+    private final Scanner scanner;
+    private final GestorBiblioteca gestor;
+
+    public Menu_consola() {
+        this.scanner = new Scanner(System.in);
+        this.gestor = new GestorBiblioteca();
+    }
+
+    public void inciar () {
+        int opcion;
+    }
+
+    do
 }
