8db27d8c257856d0a0cd9d1ef04336c52902ed1d
Angel <davidasdadad@gmail.com>
2026-06-29 16:34:59 -0500

    fix(angel): agregar metodo getCodigoLibro en Solicitud

--- src/Model/Solicitud.java
+++ src/Model/Solicitud.java
@@ -1,4 +1,11 @@
 package Model;
 
 public class Solicitud {
+
+    private int codigoLibro;
+
+    // Metodo de lectura para obtener el codigo del libro
+    public int getCodigoLibro() {
+        return codigoLibro;
+    }
 }
