258343b54b8216f50b8258fa63a06a46368a5330
Alvaro <alvaro.montoya.m.m@gmail.com>
2026-06-29 17:48:57 -0500

    switch case

--- src/View/Menu_consola.java
+++ src/View/Menu_consola.java
@@ -20,5 +20,40 @@ public class Menu_consola {
         int opcion;
     }
 
-    do
-}
+    do {
+        mostrarMenu();
+        opcion= leerEnteroEnRango(" Seccleccion una ipcion: ",0, 9)
+        switch (opcion) {
+            case 1:
+                registrarLibro();
+                break;
+            case 2:
+                buscarLibro();
+                break;
+            case 3:
+                eliminarLibro();
+                break;
+            case 4:
+                mostrarLibros();
+                break;
+            case 5:
+                registrarSolicitud();
+                break;
+            case 6:
+                atenderSolicitud();
+                break;
+            case 7:
+                registrarDevolucion();
+                break;
+            case 8:
+                generarReporte();
+                break;
+            case 9:
+                cargarDatosDePrueba();
+                break;
+            case 0:
+                System.out.println("Saliendo del sistema...");
+                break;
+            default:
+                System.out.println("Opcion no valida.");
+        }
