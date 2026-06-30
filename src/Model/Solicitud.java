6c7582a8c42dd4df55a1b3dc6460ca9fb23879b9
lohead1 <ferrohuanchialvaro@gmail.com>
2026-06-29 22:36:52 -0500

    refactor : Agregar Atributos faltantes a la clase Solicitud Se agrego el codigo estudiante, el nombre estudiante, y la fecha de Solicitud, ademas de sus metodos de accessos

--- src/Model/Solicitud.java
+++ src/Model/Solicitud.java
@@ -1,9 +1,42 @@
 package Model;
 
+import java.time.LocalDate;
+
 public class Solicitud {
+    private int codEst;
+    private String nomEst;
     private int codigoLibro;
+    private LocalDate fechaSolicitud;
+
+    private Solicitud(int codEst,String nomEst,int codigoLibro,LocalDate fechaSolicitud){
+        this.codEst= codEst;
+        this.nomEst = nomEst;
+        this.codigoLibro = codigoLibro;
+        this.fechaSolicitud = fechaSolicitud;
+    }
     // Metodo de lectura para obtener el codigo del libro
     public int getCodigoLibro() {
         return codigoLibro;
+    }
+    public int getCodEst() {
+        return codEst;
+    }
+    public LocalDate getFechaSolicitud() {
+        return fechaSolicitud;
+    }
+    public String getNomEst() {
+        return nomEst;
+    }
+    public void setCodEst(int codEst) {
+        this.codEst = codEst;
+    }
+    public void setCodigoLibro(int codigoLibro) {
+        this.codigoLibro = codigoLibro;
+    }
+    public void setFechaSolicitud(LocalDate fechaSolicitud) {
+        this.fechaSolicitud = fechaSolicitud;
+    }
+    public void setNomEst(String nomEst) {
+        this.nomEst = nomEst;
     }
 }
